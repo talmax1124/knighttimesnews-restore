850b14801668f4989b088b71703ee8496cf37b26
talmax1124 <carlitosdiazplaza@gmail.com>
2021-09-10 15:27:29 -0400

    Something

--- .eleventy.js
+++ .eleventy.js
@@ -9,10 +9,17 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/_redirects");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
 
-  eleventyConfig.setFrontMatterParsingOptions({
-    excerpt: true,
-    excerpt_separator: "<!--more-->",
-  });
+  module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addFilter("getAuthor", (authors,label) => {
+      let author = authors.filter(a => a.key === label)[0];
+      return author;
+    });
+  
+    eleventyConfig.addFilter("getPostsByAuthor", (posts,author) => {
+      return posts.filter(a => a.data.author === author);
+    });
+  };
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
