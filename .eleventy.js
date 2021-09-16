6ad533e02e229da4201ce18a17f9ff5b8538ce59
talmax1124 <carlitosdiazplaza@gmail.com>
2021-09-16 18:46:00 -0400

    Try Again

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 const { DateTime } = require("luxon");
 const readingTime = require("eleventy-plugin-reading-time");
 
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./build/styles.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
@@ -11,11 +10,27 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/_redirects");
   eleventyConfig.addPassthroughCopy("./src/robots.txt");
 
+  
+  eleventyConfig.addCollection("authors", (collection) => {
+    const blogs = collection.getFilteredByGlob("articles/*.md");
+    return blogs.reduce((coll, post) => {
+      const author = post.data.author;
+      if (!author) {
+        return coll;
+      }
+      if (!coll.hasOwnProperty(author)) {
+        coll[author] = [];
+      }
+      coll[author].push(post.data);
+      return coll;
+    }, {});
+  });
+
   // Insert current year
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   eleventyConfig.addPlugin(readingTime);
-  
+
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd LLL yyyy"
